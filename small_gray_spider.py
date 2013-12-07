a9650fb67004d9a75cfcadfadb51ccb5707e3b3d
James <jimmysalvatore.uc@gmail.com>
2013-12-06 18:25:42 -0600

    First commit.
    First commit; first attempt at a spider, using urllib2 and some regular
    expressions.  Generally unoptimized; generally not error-resistant; etc.
    Works for most things I've tried, but not streamlined for the user.
    
    Included is an example of a function you can call with it: the get_title
    function will take the title off of the web page and return it to you,
    for each page in your crawled list.
    
    TODO: Everything.

---
+++ small_gray_spider.py
@@ -0,0 +1,49 @@
+import urllib2 as url
+import re
+
+class sgs:
+
+    def __init__(self, base_url):
+        self.base_url = base_url
+        self.list_of_code = []
+        self.already_visited = [self.base_url]
+        
+        self.list_of_code = [url.urlopen(self.base_url).read()]
+        self.first_conn = self._get_first_connections_urls(self.list_of_code[0])
+        
+    def _get_first_connections_urls(self, _source_url):
+        pattern = r"""(<a href=.)(.*?)(['"])"""
+        _list = re.findall(pattern, _source_url)
+        _list_urls = [item[1] for item in _list]
+        return _list_urls
+        
+    def _get_first_connections_code(self, _list_urls):       
+
+        try:        # if a list of urls...
+            for addr in _list_urls:
+                if addr in self.already_visited: pass
+                else:                
+                    try: 
+                        self.list_of_code.append(url.urlopen(addr).read())
+                        self.already_visited.append(addr)
+                    except: pass
+            
+        except:     # if a single element or an error...
+            if addr in self.already_visited: pass
+            else:            
+                try:    # if a single element...
+                    self.list_of_code += [url.urlopen(_list_urls).read()]
+                    self.already_visited.append(addr)
+                except: print "Error in reading url {}.".format(_list_urls)
+            
+        
+        
+k = sgs("https://news.ycombinator.com/news")
+def get_title(_list):
+    biglist = []
+    for i in _list:
+        k = re.findall(r"(<head>.*?<title>)(.*?)(</)", i, re.DOTALL)
+        biglist += [i[1] for i in k]
+    return biglist
+    
+    
